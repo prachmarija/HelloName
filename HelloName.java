6acf09a6ee72fa2681e382a80f765cb0430ebe10
Mariia Dobberstein <Mariia_Dobberstein@epam.com>
2017-02-07 14:21:41 +0100

    Task 2. First version

---
+++ HelloName.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+public class HelloName {
+
+    public static void main(String[] args) {
+
+                               String name;
+
+                              //Create scanner to obtain user input
+                                      Scanner S = new Scanner( System.in );
+
+                    //obtain user input
+                                       System.out.print("Enter your name: ");
+                                       name = S.nextLine();
+
+
+                                        //output information
+                                                System.out.print("Hello " + name + "!");
+
+                           }
+
+
+         }
+
